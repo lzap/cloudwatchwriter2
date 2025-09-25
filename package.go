081fc99f6cbbc54b2c03094899bf73852b71b1d7
Lukas Zapletal <lzap+git@redhat.com>
2025-09-25 12:04:10 +0200

    fix: reset batch size after sending a batch
    When sending a batch of metrics to CloudWatch, the batch size was not
    reset after sending, leading to incorrect batch size calculations for
    subsequent batches. This commit adds a line to reset the batch size
    after sending a batch, ensuring accurate tracking of the current batch size.
    
    Also a bit of refactoring of constants for easier testing.

---
+++ package.go
@@ -0,0 +1,6 @@
+// Package cloudwatchwriter2 provides an io.Writer implementation that sends logs to AWS CloudWatch Logs.
+//
+// See: https://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html
+// and: https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/cloudwatch_limits_cwl.html for limits, quotas and
+// best practices.
+package cloudwatchwriter2
